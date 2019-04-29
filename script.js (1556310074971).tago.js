cb9d9663a041e1dabbcfe65c4adda9258df4e248
depaulobrunna <brunna@ifce.edu.br>
2019-04-29 17:21:38 -0300

    leitura de 1 byte do primeiro byte feita corretamente

--- script.js (1556310074971).tago.js
+++ script.js (1556310074971).tago.js
@@ -16,40 +16,38 @@ async function myAnalysis(context, scope) {
   // Create a variable called payload with the value sent by the device
   // change payload to the name of you variable here ||
   //                                                 \/
-  context.log('analysis started')
+  context.log('1 analysis started')
 
   const env_vars = Utils.env_to_obj(context.environment);
-  if (!env_vars.fc_token) return context.log('Missing device_token environment variable');
+  if (!env_vars.fctoken) return context.log('Missing device_token environment variable');
+
   // Instantiate the device with your device token
-  const device = new Device(env_vars.fc_token);
+  const device = new Device(env_vars.fctoken);
   var data = {}
   data = scope.find(x => x.variable === "payload").value
-  context.log(`Data is ${data}`);
-  //const payload = Number(data);
-  //context.log(`payload is ${payload}`);
+  context.log(`2 Data is ${data}`);
+  var payload = {}
   payload = Buffer.from(data, 'base64').toString('utf8');
-  context.log(`payload is ${payload}`);
+  context.log(`3 payload is ${payload}`);
 
-  // Create separate the string into two hexadecimal values
-  var values = Buffer.from(data, 'base64'); //nao ta convertendo
-  context.log(`value ${values}`);
-  
-  var len = values.readUInt1BE(0);
-  context.log(`len ${len}`);
-  
-  // Create the two variables Temperature and Humidity to send to TagoIO
-  //const variables = [{
-    //variable: 'RADIO ID',
-    //value: status
-  //}];
+    // Create separate the string into two hexadecimal values
+    var values = Buffer.from(data, 'base64');
+    context.log(`4 values ${values}`);
+    
+    var length = values.readUInt8(0);
+    context.log(`5 length is ${length}`);
+    context.log('6')
+    // Create the two variables Temperature and Humidity to send to TagoIO
+    const variables = [{
+      variable: 'length',
+      value: length,
+      unit: 'H'
+    }];
+    context.log('7')
 
-  // Insert the actual variables temperature and humidity to TagoIO
-  //await device.insert(variables).then(context.log).catch(context.log);
-
-
-  // Insert the actual variables temperature and humidity to TagoIO
-  device.insert(variables).then(context.log('Successfully Inserted')).catch(error => context.log('Error when inserting:', error));
-  context.log('Ga analysis finished')
+    // Insert the actual variables temperature and humidity to TagoIO
+    device.insert(variables).then(context.log('Successfully Inserted')).catch(error => context.log('Error when inserting:', error));
+    context.log('Ga analysis finished')
   
 
 }
