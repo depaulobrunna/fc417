d44aafb21004388847f2c94e0edb9635619b9925
Brunna <depaulobr@gmail.com>
2019-05-06 16:38:21 -0300

    conversao de bcd para numero

--- script.js (1556310074971).tago.js
+++ script.js (1556310074971).tago.js
@@ -10,54 +10,60 @@ const Analysis = require('tago/analysis');
 const Device = require('tago/device');
 const Utils = require('tago/utils');
 
+var bcd2number = function(bcd) 
+{
+    var n = 0;
+    var m = 1;
+    for(var i = 0; i<bcd.length; i+=1) {
+        n += (bcd[bcd.length-1-i] & 0x0F) * m;
+        n += ((bcd[bcd.length-1-i]>>4) & 0x0F) * m * 10;
+        m *= 100;
+    }
+    return n;
+}
 
-// The function myAnalysis will run when you execute your analysis
 async function myAnalysis(context, scope) {
-  // Create a variable called payload with the value sent by the device
-  // change payload to the name of you variable here ||
-  //                                                 \/
   context.log('analysis started')
 
   const env_vars = Utils.env_to_obj(context.environment);
   if (!env_vars.fctoken) return context.log('Missing device_token environment variable');
 
-  // Instantiate the device with your device token
   const device = new Device(env_vars.fctoken);
   var data = {}
   data = scope.find(x => x.variable === "payload").value
-  //context.log(`2 Data is ${data}`);
-  //const payload = Number(data);
-  //context.log(`payload is ${payload}`);
+  
   var payload = {}
   payload = Buffer.from(data, 'base64').toString('utf8');
   //context.log(`3 payload is ${payload}`);
-
-    // Create separate the string into two hexadecimal values
     var values = Buffer.from(data, 'base64');
-    //context.log(`4 values ${values}`);
+    //context.log(`values is ${values}`);
+    var today_value = values.readUIntBE(15, 1).toString(2); //buf.readUIntLE(offset, byteLength) suports 6 bytes, tostring(2) converte em binario
+    var yesterday_value = values.readUIntBE(21, 1).toString(2);
+    var before_yesterday_value = values.readUIntBE(27, 1).toString(2);
+    //context.log(`today value ${today_value}`);
+    //context.log(`read1`);
+    today = bcd2number(today_value);
+    yesterday = bcd2number(yesterday_value);
+    before_yesterday = bcd2number(before_yesterday_value);
     
-    var pack1 = values.readUInt8(0);
-    //var pack2 = values.readUInt8(42);
-    //var pack3 = values.readUInt8(43);
-
-    context.log(`length ${pack1}`);
-    //context.log(`6 ${pack2}`);
-    //context.log(`7 ${pack3}`);
-
-    context.log('pass')
-    // Create the two variables Temperature and Humidity to send to TagoIO
-    const variables = [{
-      variable: 'id',
-      value: pack1
-    }];
-    context.log('finish')
+   const variables = [{
+      variable: 'valor_atual',
+      value: vari,
+      unit: 'm3'
+    //}, {
+      //variable: 'yesterday_value',
+      //value: yesterday_value,
+      //unit: 'Hz'
+    //}, {
+      //variable: 'before_yesterday_value',
+      //value: before_yesterday_value,
+      //unit: 'Hz'
+  }];
 
     // Insert the actual variables temperature and humidity to TagoIO
     device.insert(variables).then(context.log('Successfully Inserted')).catch(error => context.log('Error when inserting:', error));
-    context.log('Ga analysis finished')
+    context.log('analysis finished')
   
 
 }
-
-// The analysis token in only necessary to run the analysis outside Tago
 module.exports = new Analysis(myAnalysis, 'YOUR-ANALYSIS-TOKEN');
