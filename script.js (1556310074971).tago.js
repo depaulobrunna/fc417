b4e905be550323441641dd533e73dc17fd68b16e
depaulobrunna <brunna@ifce.edu.br>
2019-05-08 17:09:48 -0300

    mudancas comentadas, codigo exportando valor corretor em teoria

--- script.js (1556310074971).tago.js
+++ script.js (1556310074971).tago.js
@@ -22,49 +22,71 @@ var bcd2number = function(bcd)
     return n;
 }
 
+var getFVal = function(val, multip) {
+  var multiplier = {
+    0x13: '10e-3',
+    0x14: '10e-2',
+    0x15: '10e-1',
+    0x16: '10e+0',
+    0x00: ''
+  };
+  return val + ' ' + (multiplier[multip] || multiplier[0x00]);
+}
+
 async function myAnalysis(context, scope) {
   context.log('analysis started')
-
   const env_vars = Utils.env_to_obj(context.environment);
   if (!env_vars.fctoken) return context.log('Missing device_token environment variable');
-
   const device = new Device(env_vars.fctoken);
   var data = {}
   data = scope.find(x => x.variable === "payload").value
-  
   var payload = {}
   payload = Buffer.from(data, 'base64').toString('utf8');
-  context.log(`3 payload is ${payload}`);
     var values = Buffer.from(data, 'base64');
-    context.log(`values is ${values}`);
-    var bbb = values.readUIntBE(0,1).toString(2);
-    var ccc = values.readUIntBE(1,1).toString(2);
-    var today_value = values.readUIntBE(15, 1).toString(2); //buf.readUIntLE(offset, byteLength) suports 6 bytes, tostring(2) converte em binario
-    var today_multip = values.readUIntBE(16, 1).toString(2);
-    var yesterday_value = values.readUIntBE(21, 1).toString(2);
-    var yesterday_multip = values.readUIntBE(21, 1).toString(2);
-    var before_yesterday_value = values.readUIntBE(27, 1).toString(2);
-    var before_yesterday_multip = values.readUIntBE(27, 1).toString(2);
-    context.log(`today value ${today_value} multip: ${today_multip}.`);
-    context.log(`bbb = ${bbb} and ccc = ${ccc}`);
+    //context.log(typeof values);
+    //var today_value = values.readUIntBE(17, 1).toString(2); //string em binario
+    context.log(`pass`);
+    var today_value = values.readUIntLE(17, 4).toString(16);
+    var today_value_mult = values.readUIntLE(22, 1).toString(16);
+    context.log(`before ${today_value}`);
+    //context.log(`multiplicador ${today_value_mult}`);
+    context.log(bcd2number(today_value));
+    //var today_values_buff = Buffer ([values.readUIntBE(17, 1), values.readUIntBE(18, 1), values.readUIntBE(19, 1), values.readUIntBE(20, 1)]);//dec (tipo number)
+    //var today_values_buff = Buffer ([values.readUIntBE(17, 4)]);
+    //context.log(Uint8Array.from(today_values_buff));
+    //const buf1 = Buffer.from(today_values_raw);
+    //context.log(typeof today_values_buff);
+    //context.log(Buffer.isBuffer(today_values_buff));
+    //context.log(today_values_buff.length);
+    //var data = new Bcd(new KaitaiStream(today_values_buff));
+    //fun_conv = bcd.decode(today_values_buff);
+    //context.log(`value ${fun_conv}`);
+    context.log(`pass 3`);
+    var multi = 0.001
+    var finaltoday = today_value * multi;
+    context.log(`final today test ${finaltoday}`);
+    //var yesterday_value = values.readUIntBE(21, 1).toString(2);
+    //var yesterday_multip = values.readUIntBE(21, 1);
+    //var before_yesterday_value = values.readUIntBE(27, 1).toString(2);
+    //var before_yesterday_multip = values.readUIntBE(27, 1);
+    
+   
    const variables = [{
-      variable: 'valor_atual',
-      value: vari,
+      variable: 'atual',
+      value: finaltoday,
       unit: 'm3'
     }, {
-      variable: 'valor_ontem',
-      value: yesterday,
+      variable: 'ontem',
+      value: yesterday_value,
       unit: 'm3'
     }, {
-      variable: 'valor_anteontem',
-      value: before_yesterday,
+      variable: 'anteontem',
+      value: before_yesterday_value,
       unit: 'm3'
   }];
-
     // Insert the actual variables temperature and humidity to TagoIO
     device.insert(variables).then(context.log('Successfully Inserted')).catch(error => context.log('Error when inserting:', error));
     context.log('analysis finished')
-  
 
 }
 module.exports = new Analysis(myAnalysis, '');
